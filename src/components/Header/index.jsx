e517e1a72cb11a4bb7ae7913d52fa922d471aebe
Dosylia <emma.montbarbon@outlook.fr>
2024-04-05 15:08:11 +0200

    Ajout de profils, page d'accueil stylisés

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,54 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import Logo from '../../assets/dark-logo.png'
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+`
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`
+
+const StyledLogo = styled.img`
+  height: 97px;
+  width: 260px;
+`
+
+const StyledNav = styled.nav`
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  margin-right: 30px;
+  &:hover {
+    cursor: pointer;
+  }
+`
 
 function Header() {
   return (
-    <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/survey/1">Questionnaire</Link>
-      <Link to="/freelances">Profils</Link>
-    </nav>
+    <StyledHeader>
+      <div>
+        <StyledLogo src={Logo} />
+      </div>
+      <StyledNav>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/freelances">Profils</StyledLink>
+        <StyledLink to="/survey/1" $isFullLink>
+          Faire le test
+        </StyledLink>
+      </StyledNav>
+    </StyledHeader>
   )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,70 @@
+import styled from 'styled-components'
+import Resume from '../../assets/undraw_Resume_re_hkth 1.svg'
+import colors from '../../utils/style/colors'
+import { Link } from 'react-router-dom'
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+`
+
+const StyledContentLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+`
+
+const StyledContentRight = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledH1 = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+  text-align: left;
+`
+
+const StyledCTA = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 261px;
+  height: 40px;
+  background-color: ${colors.primary};
+  color: ${colors.backgroundLight};
+  border-radius: 30px;
+  border: none;
+  text-decoration: none;
+`
+
+const StyledImage = styled.img`
+  width: 541px;
+  height: 506px;
+`
+
 function Home() {
-  return <div>Page d'accueil 🏡</div>
+  return (
+    <StyledContainer>
+      <StyledContentLeft>
+        <StyledH1>
+          Repérez vos besoin on s'occupe du reste, avec les meilleurs talents
+        </StyledH1>
+        <StyledCTA to="freelances">Faire le test</StyledCTA>
+      </StyledContentLeft>
+      <StyledContentRight>
+        <StyledImage src={Resume} />
+      </StyledContentRight>
+    </StyledContainer>
+  )
 }
 
 export default Home
